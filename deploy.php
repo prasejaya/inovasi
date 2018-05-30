bc1486302895f33e2b4cca0aa1cd9fe12080aaa6
edo@sevima.com <edo@sevima.com>
2018-05-30 11:33:05 +0700

    percobaaan

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'https://github.com/prasejaya/inovasi.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+set('shared_files', []);
+set('shared_dirs', []);
+
+// Writable dirs by web server 
+set('writable_dirs', []);
+
+
+// Hosts
+
+host('192.168.1.11')
+    ->name('debian');
+    ->set('deploy_path', '/home/www/inovasi/server1/');    
+    
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
